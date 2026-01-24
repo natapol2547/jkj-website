1c73b4d1d61e0bc92b542c7895ac65eb26f47df0
Tawan Sunflower <96534097+natapol2547@users.noreply.github.com>
2026-01-24 17:13:28 +0700

    fix: nope

--- src/routes/api/v1/signin/+server.ts
+++ src/routes/api/v1/signin/+server.ts
@@ -1,4 +1,4 @@
-import { adminAuth } from '$lib/server/admin';
+import { adminAuth } from '$lib/server/admin.js';
 import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
